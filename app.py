ef27bd2a839ea557a3c7910c9e859d2705371a5e
Amansingh1202 <Aks001235@gmail.com>
2020-05-03 12:06:35 +0530

    user details verification added

--- app.py
+++ app.py
@@ -15,17 +15,33 @@ def login():
 
 @app.route('/signup',methods=['POST'])
 def signup():
-    return render_template("signup.html")
+    signup_error = False
+    message = None
+    return render_template("signup.html",signup_error = signup_error,message=message)
 
 @app.route('/signupsuccess',methods=['POST'])
 def signupsuccess():
     username = request.form.get('username')
     email = request.form.get('email')
     password = request.form.get('passwword')
-    user = User(username=username,email=email,password=password)
-    db.session.add(user)
-    db.session.commit()
-    return render_template('signupsuccess.html')
+    user1 = User.query.filter_by(username=username).first()
+    user2 = User.query.filter_by(email=email).first()
+    if user1 == None and user2 == None:
+        user = User(username=username,email=email,password=password)
+        db.session.add(user)
+        db.session.commit()
+        return render_template('signupsuccess.html')
+    else:
+        signup_error = True
+        if user1 != None:
+            err = 'Username Taken'
+        elif user2 != None:
+            err = 'Email Taken'
+        elif user1 != None and user2 != None:
+            err = 'Username and email taken'
+        else:
+            err = None
+        return render_template('signup.html',signup_error = signup_error,message = err)
 
 @app.route('/index',methods=['POST'])
 def index():
