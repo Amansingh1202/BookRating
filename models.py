f6a3386e553db5c4f76b5741483736f2839646ad
Amansingh1202 <Aks001235@gmail.com>
2020-05-04 10:32:01 +0530

    modify database

--- models.py
+++ models.py
@@ -4,6 +4,6 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'userdetails'
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String,nullable=False)
+    username = db.Column(db.String,nullable=False,unique=True)
     email = db.Column(db.String,nullable=False)
     password = db.Column(db.String, nullable=False)
