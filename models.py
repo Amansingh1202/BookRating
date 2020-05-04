a659da88d07c9c287a95115a025436378e845708
Amansingh1202 <Aks001235@gmail.com>
2020-05-04 20:04:47 +0530

    add style to register page

--- models.py
+++ models.py
@@ -2,12 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     __tablename__ = 'userdetails'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+
 
 class Books(db.Model):
     __tablename__ = 'books'
